8c69d6d7597ad4e79ab77b4272dd13f39ffc4712
Daniel Alexander <66857879+dabl03@users.noreply.github.com>
2024-05-21 20:52:21 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  env: {
-    customKey: 'my-value',
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'out',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+ 
+module.exports = nextConfig
