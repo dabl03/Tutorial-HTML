07627df7725aec81a1ab9fba7f5e3d0b572a17df
Daniel Alexander <66857879+dabl03@users.noreply.github.com>
2024-05-21 21:30:30 -0400

    arreglo de bug, he intentar algo

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
- 
+import Layout from "./app/Layout,js"
 export default function Page() {
   return (
-    <>
+    <Layout>
       <h1>Index Page</h1>
       <p>
         <Link href="/other">Other Page</Link>
       </p>
-    </>
+    </Layout>
   )
 }
