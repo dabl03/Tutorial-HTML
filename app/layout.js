d430f3056dc0f429c9824e966ea632befa0bce34
Daniel Alexander <66857879+dabl03@users.noreply.github.com>
2024-05-21 21:14:08 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Layout =() =>{
+    return(
+        <>
+        <html lang="es">
+        <head>
+        </head>
+        <body>
+          <main></main>
+        </body>
+        </html>
+        </>
+    )
+}
+
+export default Layout;
